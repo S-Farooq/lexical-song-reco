15f5c6a54550dc866e37789006093b7a910862b2
Shaham Farooq <sfarooq@daisyintel.com>
2017-12-06 21:25:21 -0500

    attempt at bootstrap template

--- __init__.py
+++ __init__.py
@@ -1,6 +1,5 @@
 from time import time
 import numpy as np
-import pprint
 
 #import matplotlib
 #import matplotlib.pyplot as plt
@@ -8,17 +7,10 @@ import pprint
 from sklearn import metrics
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import MaxAbsScaler
 from sklearn.cross_validation import train_test_split
 from sklearn.feature_selection import VarianceThreshold
 from sklearn.ensemble import ExtraTreesClassifier
 
-pp = pprint.PrettyPrinter(indent=4)
-class MyPrettyPrinter(pprint.PrettyPrinter):
-    def format(self, object, context, maxlevels, level):
-        if isinstance(object, unicode):
-            return (object.encode('utf8'), True, False)
-        return pprint.PrettyPrinter.format(self, object, context, maxlevels, level)
 #import matplotlib
 
 #mpl.rcParams['figure.figsize'] = (20,10)
@@ -29,25 +21,22 @@ from sklearn.metrics.pairwise import euclidean_distances
 import re
 import requests  
 from bs4 import BeautifulSoup
-import nltk, pprint
+import nltk
 
 nltk.data.path.append("/home/shahamfarooq/nltk_data/")
 nltk.data.path.append("/home/shahamfarooq/miniconda2/lib/nltk_data/")
 nltk.data.path.append("/home/shahamfarooq/miniconda2/nltk_data/")
-try:
-    from nltk.tag.perceptron import PerceptronTagger
-except:
-    nltk.download('punkt')
+from nltk.tag.perceptron import PerceptronTagger
+
 import cPickle
 from io import BytesIO  
 
 np.random.seed(42)
 
 def get_normalized_and_split_data(all_data,x_names,split=0.2,by_artist=False):
-#     all_data = all_data[all_data['labels'].notnull()]
+
     if by_artist:
         artists = all_data['artist'].unique()
-        print artists
         X_train = all_data[all_data['artist'].isin(artists[:-2])][x_names].values
         y_train = list(all_data[all_data['artist'].isin(artists[:-2])]['labels'])
 
@@ -58,23 +47,7 @@ def get_normalized_and_split_data(all_data,x_names,split=0.2,by_artist=False):
         X = all_data[x_names].values
 
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=split, random_state=3)
-    # print x_names
-    #VARIANCE FILTER
-    # model = ExtraTreesClassifier()
-    # model.fit(X_train, y_train)
-    # print(model.feature_importances_)
-
-    # selector = VarianceThreshold(0.02)
-    # X_train = selector.fit_transform(X_train)
-    # X_test = selector.transform(X_test)
-
-    # x_names = [all_data.columns[i] for i in selector.get_support(indices=True)]
-    # print len(x_names), "out of", len(all_data.columns), "selected"
-    # print x_names
-
-
-    #STANDARD SCALING
-    # scaler = MaxAbsScaler()
+    
     scaler = StandardScaler()
     X_train =scaler.fit_transform(X_train)
     if len(y_test)>0:
@@ -87,26 +60,16 @@ def get_normalized_and_split_data(all_data,x_names,split=0.2,by_artist=False):
                 to_drop.append(i)
 
     to_drop=list(set(to_drop))
-    # print to_drop
-    # to_keep = np.array(set(range(X_train.shape[0])) - to_drop)
-
 
     X_train = np.delete(X_train, to_drop, axis=0)
     y_train = np.delete(y_train, to_drop, axis=0)
-    # X_train = X_train[to_keep,:]
-    # y_train = y_train[to_keep]
 
     n_samples, n_features = X_train.shape
-    # labels = y_labels
 
-
-    # print("n_samples %d, \t n_features %d"
-    #       % (n_samples, n_features))
     return X_train, X_test, y_train, y_test, scaler
 
 
 def get_fitted_clusters(X_train,X_test,y_train,y_test,x_names,user_song_name="",user_scaled_data=[],n_clusters = 5):
-    n_clusters = 5
 
     kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit(X_train)
 
@@ -139,14 +102,6 @@ def get_fitted_clusters(X_train,X_test,y_train,y_test,x_names,user_song_name="",
         MyPrettyPrinter().pprint(list(tmp_train['labels'])[:num])
         to_return = msg+str(list([str(x)+"<br>" for x in tmp_train['labels'][:num]]))+"<br>"
         
-        # if len(y_test)>0:
-        #     tmp_test = tmp[tmp['test']==1].sort_values(c,ascending=True)
-        #     print "---TEST:", pp.pprint(list(tmp_test['labels'][:num]))
-        
-        # if len(user_scaled_data)>0:
-        #     tmp_test = tmp[tmp['test']==2].sort_values(c,ascending=True)
-        #     print "---USER_TEST:", pp.pprint(list(tmp_test['labels'][:num]))
-
     df_centroid = pd.DataFrame(kmeans.cluster_centers_)
     
     print"\n PLOT OF CLUSTER QUALITIES vs YOUR SONG:"
@@ -168,9 +123,10 @@ def get_fitted_clusters(X_train,X_test,y_train,y_test,x_names,user_song_name="",
 
 
 
-def get_euc_dist(set1,set2,set1_y,set2_y,n_top=7):
+def get_euc_dist(set1,set2,set1_y,set2_y,n_top=10):
     ed_df = pd.DataFrame()
     ed = euclidean_distances(set1, set2)
+    
     for n in range(len(set1_y)):
         df=pd.DataFrame()
         df['distance']=ed[n,:]
@@ -186,13 +142,6 @@ def get_euc_dist(set1,set2,set1_y,set2_y,n_top=7):
 
     ed_df_p = ed_df_p.groupby('from').head(n_top)
     
-    # threedee = plt.figure().gca(projection='3d')
-    # threedee.scatter(ed_df_p['from_ind'], ed_df_p['to_ind'], ed_df_p['distance'])
-    # threedee.set_xlabel('from')
-    # threedee.set_ylabel('to')
-    # threedee.set_zlabel('distance')
-    # plt.show()
-
     cols = ['from','to','distance']
     ed_df = ed_df[cols]
     ed_df = ed_df.sort_values(['from','distance'],ascending=True)
@@ -203,82 +152,8 @@ def get_euc_dist(set1,set2,set1_y,set2_y,n_top=7):
     ed_df_top['rel_conf'] = (1.0-ed_df_top['rel_conf'])*100
 
     ed_df_top = ed_df_top.rename(columns={'to': 'My_Songs', 'rel_conf': 'Relative_Confidence'})
-    msg = "<br>SONGS IN MY PLAYLIST CLOSEST (Euclidean distance) TO YOURS & RELATIVE CONFIDENCE:<br>"
-    return msg+ ed_df_top[['My_Songs','Relative_Confidence']].sort_values(['Relative_Confidence'],ascending=False).to_html(index=False, header=False)
-
-def search_musix_corpus(artists, pages=2,corpus_file='lyric_corpus.p'):
-    p = re.compile('\/lyrics\/*')
-
-    webpage = "https://www.musixmatch.com"
-    url = 'https://www.musixmatch.com/search/'
-    useragent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36'
-
-    try:
-        lyric_corpus = cPickle.load(open(corpus_file, 'rb'))
-    except:
-        lyric_corpus = {}
-
-    # artists = ["the national", "Editors", "chvrches", "iron and wine", "william fitzsimmons", "vienna teng"]
-    # artists = ["oh wonder",'the shins','the killers','the strokes','bleachers']
-#     artists = ["alvvays","andrew bird","birdy","bon iver","kings of leon"]
-    # lyric_corpus = {}
-    for a in artists:
-        artist_lyrics = {}
-        searchlink = url + a
-        r  = requests.get(searchlink, headers={'User-Agent': useragent})
-        soup = BeautifulSoup(r.content, "lxml")
-        gdata = soup.find_all('a',{'class':'cover'})
-        artist_page = webpage + str(gdata[0].get('href')) 
-
-
-        for i in range(1,pages+1):
-            artist_page_num = artist_page
-            if i>1:
-                artist_page_num = artist_page+"/"+str(i)
-            # print artist_page_num
-
-            try:
-                r  = requests.get(artist_page_num, headers={'User-Agent': useragent})
-            except:
-                print "No lyrics page {i}, leaving this artist.".format(i=i)
-                break
-            soup = BeautifulSoup(r.content, "lxml")
-            gdata = soup.find_all('a',{'class':'title'})
-
-            for s in gdata:
-                slink = str(s.get('href'))
-                if p.search(slink):
-                    song_lyrics = []
-                    song_page = webpage+slink
-    #                 print song_page
-                    try:
-                        r  = requests.get(song_page, headers={'User-Agent': useragent})
-                    except:
-                        print "cant access lyric page for {song_page},.".format(song_page=song_page)
-                        continue
-                    soup = BeautifulSoup(r.content, "lxml")
-    #                 print soup
-                    song_name = soup.find_all('h1',{'class':'mxm-track-title__track'})
-                    song_name = song_name[0].text
-                    song_name = song_name[6:] + "-" + a
-                    print song_name.encode(encoding='UTF-8',errors='ignore')
-                    lyrics = soup.find_all('p',{'class':'mxm-lyrics__content'})
-    #                 print lyrics
-                    if len(lyrics)==0:
-                        print "No lyrics found for {song}".format(song=song_page)
-                    for l in lyrics:
-                        song_lyrics.append(l.text)
-
-                    song_lyrics  = "\n".join(song_lyrics)
-                    song_lyrics = song_lyrics.replace("\n\n","\n")
-    #                 print song_lyrics
-                    artist_lyrics[song_name]=song_lyrics
-    #                 break
-
-        lyric_corpus[a] = artist_lyrics
-
-    cPickle.dump(lyric_corpus, open(corpus_file, 'wb'))
-    return lyric_corpus
+    msg = "<br>Closest (lexically) songs from my playlist to your selected song:<br>"
+    return msg+ ed_df_top[['My_Songs','distance']].sort_values(['distance'],ascending=True).to_html(index=False, header=False)
 
 def search_musix_track(search_term):
     p = re.compile('\/lyrics\/*')
@@ -292,9 +167,9 @@ def search_musix_track(search_term):
         r  = requests.get(searchlink, headers={'User-Agent': useragent})
     except Exception as e:
         print "No track found...something went wrong.."
-	print str(e)
-	raise Exception(e)
-        return
+    	print str(e)
+    	return ""
+    
     soup = BeautifulSoup(r.content, "lxml")
     gdata = soup.find_all('a',{'class':'title'})
     for s in range(len(gdata)):
@@ -310,9 +185,8 @@ def search_musix_track(search_term):
             r  = requests.get(song_page, headers={'User-Agent': useragent})
         except:
             print "cant access lyric page for {song_page},.".format(song_page=song_page)
-            return
+            return ""
         soup = BeautifulSoup(r.content, "lxml")
-#                 print soup
         song_name = soup.find_all('h1',{'class':'mxm-track-title__track'})
         song_name = song_name[0].text
         song_name = song_name[6:]
@@ -334,8 +208,6 @@ def ie_preprocess(document):
     tagger = PerceptronTagger() 
     tagged = []
     sentences = document.split("\n")
-#     return sentences
-#     sentences = nltk.sent_tokenize(document)
     sentences = [nltk.word_tokenize(sent) for sent in sentences]
     for sent in sentences:
         tagged_tokens = tagger.tag(sent)
@@ -347,42 +219,15 @@ def tokenize_song(lyrics):
     tokenized_lyrics = ie_preprocess(lyrics)
     return tokenized_lyrics
 
-def tokenize_corpus(lyric_corpus_file, tokenized_corpus_file="tokenized_lyric_corpus.p"):
-    try:
-        tokenized_lyric_corpus = cPickle.load(open(tokenized_corpus_file, 'rb'))
-    except:
-        tokenized_lyric_corpus = {}
-
-    lyric_corpus = cPickle.load(open(lyric_corpus_file, 'rb'))
-    for a in lyric_corpus.keys():
-        if not (a in tokenized_lyric_corpus):
-            tokenized_lyric_corpus[a] = {}
-        print "ARTIST:", a
-        for s in lyric_corpus[a].keys():
-            if s in tokenized_lyric_corpus[a]:
-                continue
-            lyrics = lyric_corpus[a][s]
-            print "---SONG:",s
-            tokenized_lyrics = ie_preprocess(lyrics)
-            tokenized_lyric_corpus[a][s] = tokenized_lyrics
-            cPickle.dump(tokenized_lyric_corpus, open(tokenized_corpus_file, 'wb'))
-    
-    return tokenized_lyric_corpus
 
 
-try:
-    from nltk.sentiment.vader import SentimentIntensityAnalyzer
-except:
-    nltk.download('vader_lexicon')
 
-try:
-    from nltk import FreqDist
-except:
-    nltk.download('popular')
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk import FreqDist
 from collections import Counter
 import pandas as pd
-sid = SentimentIntensityAnalyzer()
 
+sid = SentimentIntensityAnalyzer()
 punc = re.compile('\p')
 
 def unique_words(text):
@@ -464,7 +309,6 @@ def get_song_data(tokenized_song):
         lyrics_sep = lyrics_sep + tmpsent
 
     if len(lyrics_sep)==0:
-#         print "invalid lyric data.."
         return [], []
 
     x_data = []
@@ -480,37 +324,6 @@ def get_song_data(tokenized_song):
     x_data = x_data + stats
     return x_data, x_names
 
-def get_corpus_dataframe(tokenized_lyric_file, output_file="dataframe_storage.csv"):
-    tokenized_lyric_corpus = cPickle.load(open(tokenized_lyric_file, 'rb'))
-    X_data = []
-    y_labels=[]
-    y_artists=[]
-    x_final_names = []
-    for a in tokenized_lyric_corpus.keys():
-        print "ARTIST:", a
-        total_songs = len(tokenized_lyric_corpus[a].keys())
-        curr_song=0
-        for s in tokenized_lyric_corpus[a].keys():
-            x_data, x_names = get_song_data(tokenized_lyric_corpus[a][s])
-            if len(x_data)==0:
-                continue
-            x_final_names =x_names
-            X_data.append(x_data)
-            y_labels.append(s)
-            y_artists.append(a)
-#             curr_song+=1
-  
-    # print x_names
-    # print X
-    # print y_labels
-    all_data = pd.DataFrame(X_data)
-    all_data.columns=x_final_names
-    all_data['labels']=y_labels
-    all_data['artist']=y_artists
-    all_data.to_csv(output_file,  encoding='utf-8')
-    print all_data.head()
-    return all_data
-
 
 from flask import Flask, request, render_template
 
@@ -520,42 +333,32 @@ app = Flask(__name__)
 def my_form():
     return render_template('index.html')
 
-@app.route('/testin')
-def index():
-    return render_template('index.html')
 
 @app.route('/', methods=['POST'])
 def main():
     
-    tlc ="tokenized_lyric_corpus.p"
-    lc='lyric_corpus.p'
     ds = "/var/www/FlaskApp/FlaskApp/dataframe_storage.csv"
-    artists = ["the national", "Editors", "chvrches", "william fitzsimmons", "vienna teng",
-        "oh wonder",'the shins','the killers','the strokes','bleachers',
-        "alvvays","andrew bird","birdy","bon iver","kings of leon"]
 
-    # lyric_corpus = search_musix_corpus(artists,pages=7,corpus_file=lc)
     user_song_name = request.form['text']
     test_lyric = search_musix_track(user_song_name)
+    if test_lyric=="":
+        return "oops, seems like the song's lyrics could not be found, insert artists name or try another song."
 
     tokenized_song = tokenize_song(test_lyric)
-    all_data = pd.read_csv(ds)
     user_data, x_names = get_song_data(tokenized_song)
 
+    all_data = pd.read_csv(ds)
+
     if len(user_data)==0:
-        print "oops, seems like the user tokenized song was not correct...error, contact me :)"
+        return "oops, seems like the user tokenized song was not correct...error, contact me :)"
 
     user_data = np.array(user_data)
     user_data = user_data.reshape(1,-1)
     X_train, X_test, y_train, y_test, scaler= get_normalized_and_split_data(all_data, x_names,split=0.0)
     user_scaled_data= scaler.transform(user_data)
-    #user_scaled_data = user_scaled_data.ravel()    
-
-    a = get_fitted_clusters(X_train,X_test,y_train,y_test,x_names,user_song_name,user_scaled_data,n_clusters = 5)
-
+    
     b = get_euc_dist(user_scaled_data,X_train,[user_song_name],y_train,n_top=5)
-    #+ """<a href="/img">Continue to cluster graph</a>"""
-    return str(a).encode(encoding='UTF-8',errors='ignore') + str(b).encode(encoding='UTF-8',errors='ignore') 
+    return str(b).encode(encoding='UTF-8',errors='ignore') 
 
 if __name__ == '__main__':
     app.run(debug=True, port=80)
