f909b97551a2630a47047e2e03176213bb243253
Shaham Farooq <sfarooq@daisyintel.com>
2017-12-06 23:39:20 -0500

    cleaned bootstrap temp

---
+++ spotify_playlist.py
@@ -0,0 +1,37 @@
+import spotipy
+
+birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
+spotify = spotipy.Spotify()
+
+results = spotify.artist_albums(birdy_uri, album_type='album')
+albums = results['items']
+while results['next']:
+    results = spotify.next(results)
+    albums.extend(results['items'])
+
+for album in albums:
+    print(album['name'])
+
+
+import sys
+import spotipy.util as util
+
+scope = 'user-library-read'
+
+if len(sys.argv) > 1:
+    username = sys.argv[1]
+else:
+    print "Usage: %s username" % (sys.argv[0],)
+    sys.exit()
+
+token = util.prompt_for_user_token(username, scope, client_id='5f535ebef74b42d58590161e7bfcf653',
+    client_secret='b9f8b9f7c055433894bdcef35a2905f0')
+
+if token:
+    sp = spotipy.Spotify(auth=token)
+    results = sp.current_user_saved_tracks()
+    for item in results['items']:
+        track = item['track']
+        print track['name'] + ' - ' + track['artists'][0]['name']
+else:
+    print "Can't get token for", username
