3de85eda494a9e8b001d0ae60aa81855b8f5ab7f
Shaham Farooq <sfarooq@daisyintel.com>
2017-12-07 15:23:05 -0500

    separated into util

--- util_fxns.py
+++ util_fxns.py
@@ -463,5 +463,5 @@ if __name__ == '__main__':
         "julien baker","yeah yeah yeahs", "angus and julia stone", "catfish and the bottlemen", "clap your hands say yeah"]
 
     lyric_corpus = search_musix_corpus(artists,pages=7,corpus_file=lc)
-    tokenized_lyric_corpus = tokenize_corpus(lyric_corpus_file, tokenized_corpus_file=tlc)
-    all_data = get_corpus_dataframe(tokenized_lyric_file, output_file=ds)
+    tokenized_lyric_corpus = tokenize_corpus(lc, tokenized_corpus_file=tlc)
+    all_data = get_corpus_dataframe(tlc, output_file=ds)
